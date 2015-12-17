851ea3233dc30b4aeb79de7666c16947a7bc40f1
Bobby Ren <bobbyren@gmail.com>
2015-12-17 12:38:31 -0500

    added Constants file with Parse constants; initializes parse from appDelegate

--- BondVoyage/AppDelegate.swift
+++ BondVoyage/AppDelegate.swift
@@ -7,6 +7,8 @@
 //
 
 import UIKit
+import Parse
+import Bolts
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -16,6 +18,15 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         // Override point for customization after application launch.
+        Parse.enableLocalDatastore()
+        
+        // Initialize Parse.
+        Parse.setApplicationId(PARSE_APP_ID,
+            clientKey: PARSE_CLIENT_KEY)
+        
+        // [Optional] Track statistics around application opens.
+        PFAnalytics.trackAppOpenedWithLaunchOptions(launchOptions)
+ 
         return true
     }
 
