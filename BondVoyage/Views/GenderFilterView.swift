a5f107d953e54ca2ff558f73004a01625fd7db23
Amy Ly <amyly@Amys-MacBook-Air.local>
2016-01-02 21:37:55 -0500

    Added uiview classes for each filter view, added them to storyboard

---
+++ BondVoyage/Views/GenderFilterView.swift
@@ -0,0 +1,13 @@
+//
+//  GenderFilterView.swift
+//  BondVoyage
+//
+//  Created by Amy Ly on 1/2/16.
+//  Copyright © 2016 RenderApps. All rights reserved.
+//
+
+import UIKit
+
+class GenderFilterView: UIView {
+
+}
---
+++ BondVoyage/Views/GroupSizeFilterView.swift
@@ -0,0 +1,13 @@
+//
+//  GroupSizeFilterView.swift
+//  BondVoyage
+//
+//  Created by Amy Ly on 1/2/16.
+//  Copyright © 2016 RenderApps. All rights reserved.
+//
+
+import UIKit
+
+class GroupSizeFilterView: UIView {
+
+}
