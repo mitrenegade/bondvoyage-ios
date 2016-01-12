fc0db9142bd597ce22672903d3da8e2a892dd5dc
Bobby <bobbyren@gmail.com>
2016-01-12 11:18:11 -0500

    formatting, colors, and added info label to user profile cells

--- BondVoyage/ViewControllers/SearchResultsViewController.swift
+++ BondVoyage/ViewControllers/SearchResultsViewController.swift
@@ -20,13 +20,35 @@ class UserSearchResultCell: UITableViewCell {
     @IBOutlet weak var profileImage: AsyncImageView!
     @IBOutlet weak var usernameLabel: UILabel!
     @IBOutlet weak var genderAndAgeLabel: UILabel!
+    @IBOutlet weak var infoLabel: UILabel!
 
     func configureCellForUser(user: PFUser) {
         let currentYear = components.year
         let age = currentYear - (user.valueForKey("birthYear") as! Int)
-        self.usernameLabel.text = user.username
+        
+        var name: String? = user.valueForKey("firstName") as? String
+        if name == nil {
+            name = user.valueForKey("lastName") as? String
+        }
+        if name == nil {
+            name = user.username
+        }
+        self.usernameLabel.text = name
         self.genderAndAgeLabel.text = "\(user.valueForKey("gender")!), age: \(age)"
 
+        var info: String? = nil
+        if let interests: [String] = user.valueForKey("interests") as? [String] {
+            if interests.count > 0 {
+                info = "Likes: \(interests[0])"
+                if interests.count > 1 {
+                    for var i=1; i < interests.count; i++ {
+                        info = "\(info!), \(interests[i])"
+                    }
+                }
+            }
+        }
+        self.infoLabel.text = info
+        
         if let photoURL: String = user.valueForKey("photoUrl") as? String {
             self.profileImage.imageURL = NSURL(string: photoURL)
         }
