b627f233b2de681a6a6b0dc30fb7902d6ffb6015
Amy Ly <amyly@Amys-MacBook-Air.local>
2016-01-16 16:30:48 -0500

    removed nav bar shadow, re-layed out user details

--- BondVoyage/UIColor+BVColors.swift
+++ BondVoyage/UIColor+BVColors.swift
@@ -9,6 +9,7 @@
 import UIKit
 
 extension UIColor {
+
     class func BV_defaultBlueColor() -> UIColor {
         return colorFromRGB(red: 79, green: 129, blue: 170)
     }
@@ -17,7 +18,20 @@ extension UIColor {
         return colorFromRGB(red: 74, green: 144, blue: 226)
     }
 
+    class func BV_backgroundGrayColor() -> UIColor {
+        return colorFromRGBWithAlpha(red: 173, green: 173, blue: 173, alpha: 0.8)
+    }
+
+    class func BV_navigationBarGrayColor() -> UIColor {
+        return colorFromRGBWithAlpha(red: 219, green: 219, blue: 219, alpha: 0.92)
+    }
+
     private class func colorFromRGB(red red: CGFloat, green: CGFloat, blue: CGFloat) -> UIColor {
         return UIColor.init(red: red/255.0, green: green/255.0, blue: blue/255.0, alpha: 1.0)
     }
+
+    private class func colorFromRGBWithAlpha(red red: CGFloat, green: CGFloat, blue: CGFloat, alpha: CGFloat) -> UIColor {
+        return UIColor.init(red: red/255.0, green: green/255.0, blue: blue/255.0, alpha: alpha)
+    }
+
 }
--- BondVoyage/ViewControllers/UserDetailsViewController.swift
+++ BondVoyage/ViewControllers/UserDetailsViewController.swift
@@ -12,20 +12,30 @@ import AsyncImageView
 
 class UserDetailsViewController: UIViewController {
 
+    var selectedUser: PFUser!
     @IBOutlet weak var nameLabel: UILabel!
     @IBOutlet weak var genderAndAgeLabel: UILabel!
     @IBOutlet weak var interestsLabel: UILabel!
     @IBOutlet weak var scrollViewContainer: AsyncImageView!
-    var selectedUser: PFUser!
-
-    
+    @IBOutlet weak var transparentView: UIView!
+    @IBOutlet weak var nameView: UIView!
+    @IBOutlet weak var interestsView: UIView!
     @IBOutlet weak var inviteToBondButton: UIButton!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         self.configureDetailsForUser()
+        self.configureUI()
+    }
+
+    func configureUI() {
         self.title = "Invite"
         self.inviteToBondButton.backgroundColor = UIColor.BV_primaryActionBlueColor()
+        self.transparentView.backgroundColor = UIColor.clearColor()
+        self.transparentView.frame = CGRectMake(0, 0, CGRectGetWidth(self.view.frame), CGRectGetHeight(self.view.frame))
+        print("\(self.transparentView.frame.height)")
+        self.nameView.backgroundColor = UIColor.BV_backgroundGrayColor()
+        self.interestsView.backgroundColor = UIColor.BV_backgroundGrayColor()
     }
 
     override func viewWillAppear(animated: Bool) {
@@ -46,6 +56,11 @@ class UserDetailsViewController: UIViewController {
 
         let firstName = self.selectedUser.valueForKey("firstName")!
         self.nameLabel.text = "\(firstName)"
+
+        let currentYear = components.year
+        let age = currentYear - (self.selectedUser.valueForKey("birthYear") as! Int)
+        self.genderAndAgeLabel.text = "\(self.selectedUser.valueForKey("gender")!), age: \(age)"
+
 
         let interests = self.selectedUser.valueForKey("interests")!
         self.interestsLabel.text = "Interests: \(interests)"
