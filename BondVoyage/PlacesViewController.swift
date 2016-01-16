9bf7bf9073e4f62a121cb3661be7eeb0cb6b4308
Bobby <bobbyren@gmail.com>
2016-01-16 14:27:22 -0500

    PlacesViewController loads recommendations from web; user can choose the next recommendation

--- BondVoyage/PlacesViewController.swift
+++ BondVoyage/PlacesViewController.swift
@@ -8,6 +8,7 @@
 
 import UIKit
 import AsyncImageView
+import Parse
 
 class PlacesViewController: UIViewController {
     
@@ -16,10 +17,15 @@ class PlacesViewController: UIViewController {
     @IBOutlet weak var buttonGo: UIButton!
     @IBOutlet weak var buttonNext: UIButton!
     
+    @IBOutlet weak var activityIndicator: UIActivityIndicatorView!
+    
+    var recommendations: [PFObject]?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        self.loadRecommendation()
     }
 
     override func didReceiveMemoryWarning() {
@@ -27,14 +33,68 @@ class PlacesViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
+    // MARK: - Query
+    func loadRecommendation() {
+        // HACK: load any recommendation
+        self.activityIndicator.startAnimating()
+        RecommendationRequest.recommendationsQuery(nil, interests: []) { (results, error) -> Void in
+            self.activityIndicator.stopAnimating()
+            if results != nil {
+                self.recommendations = results
+                self.refresh()
+            }
+            else {
+                let message = "No recommendations are available for your bond."
+                self.simpleAlert("Could not load recommendations", defaultMessage: message, error: error)
+            }
+        }
+    }
+    
+    func nextRecommendation() {
+        if self.recommendations == nil || self.recommendations!.count == 0 {
+            self.noMoreRecommendations()
+        }
+        if self.recommendations?.count > 0 {
+            self.recommendations!.removeFirst()
+            self.refresh()
+        }
+    }
+    
+    func noMoreRecommendations() {
+        self.simpleAlert("No more recommendations", defaultMessage: "We are out of recommendations. Tough luck!", error: nil)
+    }
+
+    // MARK: - Display
+    func refresh() {
+        let recommendation: PFObject = self.recommendations!.first!
+        if let url: String = recommendation.objectForKey("imageURL") as? String {
+            self.imageView.imageURL = NSURL(string: url)
+        }
+        else if let image: PFFile = recommendation.objectForKey("image") as? PFFile {
+            self.imageView.imageURL = NSURL(string: image.url!)
+        }
+
+        var text: String = ""
+        if let title: String = recommendation.objectForKey("name") as? String {
+            text = title
+        }
+        if let description: String = recommendation.objectForKey("description") as? String {
+            text = "\(text)\n\n\(description)"
+        }
+        self.labelInfo.text = text
+    }
+
+    // MARK: - Action
     @IBAction func didClickButton(button: UIButton) {
         if button == self.buttonGo {
             print("Go")
         }
         else if button == self.buttonNext {
             print("Next")
+            self.nextRecommendation()
         }
     }
+    
     /*
     // MARK: - Navigation
 
