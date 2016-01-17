80f3859d7637c6c43b00d42e3db1eca9619fedf9
Amy Ly <amyly@Amys-MacBook-Air.local>
2016-01-17 16:51:32 -0500

    accept invitation implemented as subclass of userdetailsvc

--- BondVoyage/ViewControllers/UserDetailsViewController.swift
+++ BondVoyage/ViewControllers/UserDetailsViewController.swift
@@ -13,22 +13,23 @@ import AsyncImageView
 class UserDetailsViewController: UIViewController {
 
     var selectedUser: PFUser!
+    @IBOutlet weak var scrollViewContainer: AsyncImageView!
     @IBOutlet weak var nameLabel: UILabel!
     @IBOutlet weak var genderAndAgeLabel: UILabel!
     @IBOutlet weak var interestsLabel: UILabel!
-    @IBOutlet weak var scrollViewContainer: AsyncImageView!
+    @IBOutlet weak var aboutMeLabel: UILabel!
     @IBOutlet weak var transparentView: UIView!
     @IBOutlet weak var nameView: UIView!
     @IBOutlet weak var interestsView: UIView!
     @IBOutlet weak var inviteToBondButton: UIButton!
-
-    @IBOutlet weak var aboutMeLabel: UILabel!
-
     @IBOutlet weak var interestsToTransparentViewSpacingConstraint: NSLayoutConstraint!
+
     override func viewDidLoad() {
         super.viewDidLoad()
         self.configureDetailsForUser()
         self.configureUI()
+
+        self.inviteToBondButton.addTarget(self, action:"primaryActionButtonPressed:", forControlEvents: UIControlEvents.TouchUpInside)
     }
 
     func configureUI() {
@@ -38,13 +39,6 @@ class UserDetailsViewController: UIViewController {
         self.nameView.backgroundColor = UIColor.BV_backgroundGrayColor()
         self.interestsView.backgroundColor = UIColor.BV_backgroundGrayColor()
         self.interestsToTransparentViewSpacingConstraint.constant = (self.nameView.bounds.height - 1) // I don't know why there is a 2 pixel gap between views
-    }
-
-    override func viewWillAppear(animated: Bool) {
-
-    }
-
-    @IBAction func inviteToBondButtonPressed(sender: UIButton) {
     }
 
     func configureDetailsForUser() {
@@ -62,25 +56,31 @@ class UserDetailsViewController: UIViewController {
         let age = currentYear - (self.selectedUser.valueForKey("birthYear") as! Int)
         self.genderAndAgeLabel.text = "\(self.selectedUser.valueForKey("gender")!), age: \(age)"
 
+        self.configureInterestsLabel()
 
-        let interests = self.selectedUser.valueForKey("interests")!
-        self.interestsLabel.text = "Interests: \(stringFromArray(interests as! Array<String>))"
         self.aboutMeLabel.text = "About me: Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
     }
 
-    private func stringFromArray(arr: Array<String>) -> String {
+    func configureInterestsLabel() {
+        let interests = self.selectedUser.valueForKey("interests")!
+        self.interestsLabel.text = "Interests: \(stringFromArray(interests as! Array<String>))"
+    }
+
+    func pimaryActionButtonPressed(sender: UIButton) {
+        print("User pressed invite to bond")
+    }
+
+    // MARK: - Helper Methods
+
+    func stringFromArray(arr: Array<String>) -> String {
         var interestsString = String()
-        for interest in arr { //TODO: this is o(n) where n time with a lot of string appending, so prob not the best way
+        for interest in arr {
             if interestsString.characters.count == 0 {
                 interestsString = interest
             } else {
                 interestsString = interestsString + ", " + interest
             }
         }
-        print(interestsString)
         return interestsString
     }
-
-
-
 }
