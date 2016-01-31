e8fe2df90d1e1f1993124b9014426c6f8a5346d3
Bobby <bobbyren@gmail.com>
2016-01-31 13:54:08 -0500

    user details displays about

--- BondVoyage/ViewControllers/UserDetailsViewController.swift
+++ BondVoyage/ViewControllers/UserDetailsViewController.swift
@@ -118,18 +118,31 @@ class UserDetailsViewController: UIViewController {
             return
         }
 
-        if let interests = user!.valueForKey("interests") as? [String] {
-            if self.selectedUser != nil {
-                self.interestsLabel.text = "Interests: \(stringFromArray(interests))"
-            }
-            else {
-                self.interestsLabel.text = "Wants to bond over: \(stringFromArray(interests))" // todo: load match and set this to match category
-            }
+        if self.invitingMatch != nil {
+            self.invitingMatch!.fetchInBackgroundWithBlock({ (object, error) -> Void in
+                if let categories: [String] = self.invitingMatch!.objectForKey("categories") as? [String] {
+                    let str = self.stringFromArray(categories)
+                    if self.selectedUser != nil {
+                        self.interestsLabel.text = "Interests: \(str)"
+                    }
+                    else {
+                        self.interestsLabel.text = "Wants to bond over: \(str)" // todo: load match and set this to match category
+                    }
+                }
+            })
+        }
+        else {
+            self.interestsLabel.text = nil
         }
 
-        self.aboutMeLabel.text = "About me: Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+        if let about = user!.valueForKey("about") as? String {
+            self.aboutMeLabel.text = "About me: \(about)"
+        }
+        else {
+            self.aboutMeLabel.text = nil
+        }
     }
-    
+
     func dismiss() {
         self.navigationController?.popViewControllerAnimated(true)
     }
