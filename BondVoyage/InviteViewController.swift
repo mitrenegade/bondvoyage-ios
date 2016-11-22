779c187714b84d72f0ea904d34f9f42d23f96f59
Bobby <bobbyren@gmail.com>
2016-11-21 20:52:16 -0500

    goes to chat if activity already contains user as an invitee

--- BondVoyage/InviteViewController.swift
+++ BondVoyage/InviteViewController.swift
@@ -78,7 +78,10 @@ class InviteViewController: UIViewController {
         
         let activityId = activity.objectId
         Activity.inviteToJoinActivity(activityId: activityId!, inviteeId: selectedUser.objectId!, completion:{ (activity, error) in
-            print("results")
+            print("results \(activity?.invitee), my id: \(user.objectId!)")
+            if let invitees = activity?.invitee as? [String], invitees.contains(user.objectId!)  {
+                self.goToChat(selectedUser)
+            }
         })
     }
     
